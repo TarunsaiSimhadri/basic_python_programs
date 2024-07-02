64b958631f53e3f3b5c856131d0e329effd12047
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-02 15:55:32 +0530

    [TarunSai] Add: Python program to print even number or not

---
+++ Basic core programs/program8.py
@@ -0,0 +1,16 @@
+"""
+
+@Author: TarunSai
+@Date: 2024-06-26
+@Last Modified by: Tarunsai
+@Last Modified time: 2024-06-27
+@Title : Program to print a number is even or not.
+
+"""
+
+n=int(input("enter n value:"))
+
+if n%2==0:
+    print(n, "is even number")
+else:
+    print(n, "is a odd number")
