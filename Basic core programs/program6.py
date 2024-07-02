23aafcb3adb2263b37943e1ac2fea5c81225309d
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-02 15:51:51 +0530

    [TarunSai] Add: Python program to find quptient and remainder

---
+++ Basic core programs/program6.py
@@ -0,0 +1,18 @@
+"""
+
+@Author: TarunSai
+@Date: 2024-06-26
+@Last Modified by: Tarunsai
+@Last Modified time: 2024-06-27
+@Title : Program to find remainder and quotient of the given number.
+
+"""
+
+dividend = int(input("enter dividend:"))
+divisor = int(input("enter divisor:"))
+
+quotient = dividend / divisor
+remainder = dividend % divisor
+
+print("quotient is", quotient)
+print("remainder is", remainder)
